bdfb884876223061226061e55a0900dba3184fb1
ELİTE <efe113878@gmail.com>
2026-05-17 02:07:59 +0300

    Update gui.c

--- gui.c
+++ gui.c
@@ -1,15 +1,15 @@
 #include "wind_subsystem.h"
 
 void draw_window_pure(int x, int y, int width, int height, uint32_t border_color) {
-    // Üst ve Alt Çizgileri Piksel Piksel Çiz
+    /* Üst ve Alt Çizgiler */
     for (int i = 0; i < width; i++) {
-        draw_pixel_pure(x + i, y, border_color);          // Üst kenar
-        draw_pixel_pure(x + i, y + height, border_color); // Alt kenar
+        draw_pixel_pure(x + i, y, border_color);
+        draw_pixel_pure(x + i, y + height, border_color);
     }
     
-    // Sağ ve Sol Çizgileri Piksel Piksel Çiz
+    /* Sağ ve Sol Çizgiler */
     for (int i = 0; i < height; i++) {
-        draw_pixel_pure(x, y + i, border_color);         // Sol kenar
-        draw_pixel_pure(x + width, y + i, border_color); // Sağ kenar
+        draw_pixel_pure(x, y + i, border_color);
+        draw_pixel_pure(x + width, y + i, border_color);
     }
 }
