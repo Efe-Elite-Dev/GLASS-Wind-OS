bc5c05a3e96e54e3643e10513c9493453f5e115c
ELİTE <efe113878@gmail.com>
2026-05-20 00:19:12 +0300

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,37 +1,29 @@
 section .multiboot
 align 4
-    MULTIBOOT_MAGIC    equ 0x1BADB002
-    MULTIBOOT_FLAGS    equ 0x00000003 ; SADECE METİN MODU (0x07 olan grafik modunu iptal ettik)
-    MULTIBOOT_CHECKSUM equ -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS)
+    MAGIC equ 0x1BADB002
+    FLAGS equ 0x03 ; Sadece sayfa hizalama ve hafıza bilgisi (Grafik KAPALI)
+    CHECKSUM equ -(MAGIC + FLAGS)
 
-    dd MULTIBOOT_MAGIC
-    dd MULTIBOOT_FLAGS
-    dd MULTIBOOT_CHECKSUM
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
 
-    ; EĞER BURADA ALT KISIMDA "dd 1024", "dd 768" GİBİ SATIRLAR VARSA ONLARI TAMAMEN SİL.
-    ; Text modunda onlara ihtiyacımız yok, silmezsen GRUB yine kafayı yer.
-
-; --- STACK ALANI (Aynı kalacak) ---
 section .bss
 align 16
 stack_bottom:
-    resb 16384 
+    resb 16384 ; Kernel için 16 KB temiz yığın alanı
 stack_top:
 
-; --- GİRİŞ NOKTASI (Aynı kalacak) ---
 section .text
 global _start
-extern kernel_main 
+extern kernel_main
 
 _start:
-    cli 
-    mov esp, stack_top 
+    cli ; Kesmeleri kapat
+    mov esp, stack_top ; Stack'i ayarla
 
-    push ebx 
-    push eax 
-
-    call kernel_main
+    call kernel_main ; C koduna geç
 
 .hang:
-    hlt 
+    hlt ; İşlemciyi uyut
     jmp .hang
