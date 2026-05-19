628b377e9be629441ab6baae3061f2d81d201687
ELİTE <efe113878@gmail.com>
2026-05-19 20:59:05 +0300

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,9 +1,24 @@
 #include "globals.h"
 #include "gui.h"
 
-void kernel_main() {
+// Varsayılan sanal makine LFB adresi (Aşağıda GRUB'dan gelene göre güncellenecek)
+uint32_t* gfx_framebuffer = (uint32_t*)0xE0000000; 
+
+SystemState current_state = STATE_WELCOME;
+bool ai_hud_visible = false;
+SetupData os_setup_data;
+
+void kernel_main(uint32_t magic, uint32_t* mbi) {
+    // GRUB üzerinden başarıyla boot edildiyse ve grafik bilgisi varsa
+    if (magic == 0x2BADB002 && mbi != NULL) {
+        // Multiboot yapısının 18. elemanı (offset 72) bize ekran kartının GERÇEK adresini verir
+        uint32_t real_fb = mbi[18]; 
+        if (real_fb != 0) {
+            gfx_framebuffer = (uint32_t*)real_fb;
+        }
+    }
+
     init_vga();
-    
     os_setup_data.wifi_connected = false;
 
     while (true) {
@@ -11,15 +26,12 @@ void kernel_main() {
             case STATE_WELCOME:
                 draw_setup_welcome();
                 break;
-                
             case STATE_LOCATION:
                 draw_setup_location();
                 break;
-                
             case STATE_COMPLETING:
                 draw_setup_completing();
                 break;
-                
             case STATE_DESKTOP:
                 draw_main_desktop();
                 break;
