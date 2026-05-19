892ae9faf3c4ba8c2034e310693faefe197cc3fb
ELİTE <efe113878@gmail.com>
2026-05-19 20:01:13 +0300

    Update sky_loader.c

--- sky_loader.c
+++ sky_loader.c
@@ -1,7 +1,7 @@
+#include "globals.h"
 #include "sky_loader.h"
 #include "gui.h"
 
-// Yerel strstr (Dış bağımlılıkları sıfırlamak için)
 static int match_extension(const char* str, const char* ext) {
     int i = 0, j = 0;
     while (str[i] != '\0') {
@@ -30,19 +30,19 @@ void sky_handle_double_click(const char* filename) {
 }
 
 void load_pe_executable(const char* path) {
-    draw_window_pure(200, 200, 400, 120, "EXE Çalısma Modülü");
-    draw_text("PE Executable yükleniyor...", 220, 250, 0x000000);
+    draw_window_pure(200, 200, 400, 120, "EXE Calisma Modulu");
+    draw_text("PE Executable yukleniyor...", 220, 250, 0x000000);
     draw_text(path, 220, 270, 0x0000FF);
 }
 
 void unpack_deb_package(const char* path) {
     draw_window_pure(200, 200, 400, 120, "DEB Paket Kurucu");
-    draw_text("Debian paketi ayıklanıyor ve kuruluyor...", 220, 250, 0x000000);
+    draw_text("Debian paketi ayiklaniyor...", 220, 250, 0x000000);
     draw_text(path, 220, 270, 0x33AA33);
 }
 
 void execute_sky_bundle(const char* path) {
-    draw_window_pure(200, 200, 400, 120, "SKY Native Uygulaması");
-    draw_text("Özel .sky paketi basarıyla baslatıldı.", 220, 250, 0x000000);
+    draw_window_pure(200, 200, 400, 120, "SKY Native Uygulamasi");
+    draw_text("Ozel .sky paketi baslatildi.", 220, 250, 0x000000);
     draw_text(path, 220, 270, 0xFF00FF);
 }
