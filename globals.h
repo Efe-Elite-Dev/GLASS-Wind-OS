7dee49169e6df7c0840401fbde17a2642bec651d
ELİTE <efe113878@gmail.com>
2026-05-19 18:09:45 +0300

    Update globals.h

--- globals.h
+++ globals.h
@@ -1,10 +1,13 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-#include <stdint.h>
-
-// Tüm dosyaların ortak erişeceği grafik değişkenleri
-extern uint32_t* GRAPHICS_FRAMEBUFFER;
-extern int is_graphics_mode;
+#define GRAPHICS_FRAMEBUFFER 0xA0000 
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef int bool;
+#define true 1
+#define false 0
+#define NULL ((void*)0)
 
 #endif
