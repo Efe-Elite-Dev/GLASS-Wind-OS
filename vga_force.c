17f15a4e62f4e52feb26b42283a13ee8b8e907d2
ELİTE <efe113878@gmail.com>
2026-05-19 19:58:01 +0300

    Delete vga_force.c

--- vga_force.c
+++
@@ -1,8 +0,0 @@
-#include "sky_core.h"
-
-void force_graphics_hardware(void) {
-    // Burada GRAPHICS_FRAMEBUFFER'ı kullanabilirsin, hata vermeyecek.
-    if (GRAPHICS_FRAMEBUFFER == 0) {
-        // Hata işleme
-    }
-}
